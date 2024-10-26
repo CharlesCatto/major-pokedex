d097c242231f3993e5574f7162bc785836989f7b
Charles Catto <charlescatto@outlook.fr>
2024-10-26 22:25:20 +0200

    Quest 3 done

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,29 @@
+function PokemonCard() {
+  const pokemon = pokemonList[0].imgSrc ? (
+    <img src={pokemonList[0].imgSrc} alt={pokemonList[0].name} />
+  ) : (
+    <p>???</p>
+  );
+
+  return (
+    <figure>
+      {pokemon}
+      <figcaption>{pokemonList[0].name}</figcaption>
+    </figure>
+  );
+}
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    name: "mew",
+  },
+];
+
+export default PokemonCard;
