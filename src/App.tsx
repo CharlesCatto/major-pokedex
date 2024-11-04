cde298223a32a9c2af2c5f0bdf1fcc19366c081e
Charles Catto <charlescatto@outlook.fr>
2024-11-04 12:05:29 +0100

    Continuing quest 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickNext = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
+  const handleClickPrevious = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
   return (
     <div>
       <PokemonCard pokemon={pokemonList[0]} />
+      <div>
+        <button type="button" onClick={handleClickPrevious}>
+          Précédent
+        </button>
+        <button type="button" onClick={handleClickNext}>
+          Suivant
+        </button>
+      </div>
     </div>
   );
 }
